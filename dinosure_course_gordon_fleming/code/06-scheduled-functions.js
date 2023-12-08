6aa8085b912c7f9a5aaf3cb850afde611c19e77d
Gordon Fleming <flemingrgordon@gmail.com>
2023-12-08 16:43:04 +0200

    part6

--- dinosure_course_gordon_fleming/code/06-scheduled-functions.js
+++ dinosure_course_gordon_fleming/code/06-scheduled-functions.js
@@ -1,1 +1,37 @@
 // 06-scheduled-functions
+/**
+ * Executed on the schedule defined in `.root-config.json`.
+ * @param {object} params
+ * @param {PlatformPolicy} params.policy The policy for which the scheduled function is running.
+ * @param {PlatformPolicyholder} params.policyholder The policyholder linked to the policy
+ * @return {ProductModuleAction[] | void} The actions to be queued by the platform.
+ */
+const applyAnnualIncrease = ({ policy, policyholder }) => {
+  // Check if the policy is older than a year
+  const policyStartDate = moment(policy.start_date);
+  const today = policy.module.today ?? moment();
+  const policyAge = today.diff(policyStartDate, 'years');
+
+  // Check for older than a year and 1st of January
+  if (policyAge >= 1 && today.month() === 0 && today.date() === 1) {
+    // Increase the cover by R10 000
+    const newCover = policy.sum_assured + 10000 * 100;
+
+    // Update the cover amount to have the new premium calculated
+    policy.sum_assured = newCover;
+    policy.module.cover_amount = newCover;
+    const newPremium = corePremiumCalc(policy.module);
+
+    return [
+      {
+        name: 'update_policy',
+        data: {
+          monthlyPremium: newPremium,
+          sumAssured: newCover,
+        },
+      },
+    ];
+  } else {
+    return null;
+  }
+};
---
+++ dinosure_course_gordon_fleming/code/unit-tests/06-scheduled-functions-data.js
@@ -0,0 +1,22 @@
+// Policy data to test scheduled functions
+const validPolicy = {
+  status: 'active',
+  package_name: 'DinoSure Protection',
+  sum_assured: 9000000,
+  base_premium: 145800,
+  monthly_premium: 145800,
+  // 1st January 2023
+  start_date: '2023-01-01',
+  end_date: null,
+  module: {
+    type: 'dinosure_course_gordon_fleming',
+    start_date: '2023-12-12T09:47:52.380Z',
+    cover_amount: 9000000,
+    birth_date: '2003-12-05T09:47:52.381Z',
+    species: 'Tyrannosaurus Rex',
+    health_checks_updated: true,
+    dinosaur_name: 'Dino',
+    dinosaur_colour: 'Lilac',
+    ndrn: '123456',
+  },
+};
